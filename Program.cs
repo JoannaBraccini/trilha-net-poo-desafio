c9832699a6b49a6c8b1cffb00d87978a0e920257
Joanna Braccini <joannabraccini@gmail.com>
2025-06-07 18:59:08 -0300

    Atualiza projeto para .NET 9.0 e implementa classes de Smartphone com métodos de instalação de aplicativos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new("123456789", "iPhone 13", "123456789012345", 128);
+Nokia nokia = new("987654321", "Nokia 3310", "987654321098765", 16);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+Console.WriteLine("Testes concluídos com sucesso!");
